642032a8e1c0df76b05345b637a85212c8f3a865
Luiz Almeida <luizorst@hotmail.com>
2020-09-12 23:23:58 -0300

    more formatting

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import os, fitz
 
+
 print(os.getcwd())
 
-imgdir= input("Digite o caminho para a pasta onde estão as imagens: ")
-name=input("Digite o nome da saída: ")
-
+imgdir = input("Digite o caminho para a pasta onde estão as imagens: ")
+name = input("Digite o nome da saída: ")
 
 imglist = os.listdir(imgdir)  # list of them
 imgcount = len(imglist)  # pic count
@@ -15,10 +15,11 @@ for i, f in enumerate(imglist):
     img = fitz.open(os.path.join(imgdir, f))  # open pic as document
     rect = img[0].rect  # pic dimension
     pdfbytes = img.convertToPDF()  # make a PDF stream
-    img.close()  # no longer needed
+    img.close()
     imgPDF = fitz.open("pdf", pdfbytes)  # open stream as PDF
-    page = doc.newPage(width = rect.width,  # new page with ...
-                       height = rect.height)  # pic dimension
+    page = doc.newPage(
+        width=rect.width, height=rect.height
+    )  # page dimension
     page.showPDFpage(rect, imgPDF, 0)  # image fills the page
 
 doc.save("{}.pdf".format(name))
